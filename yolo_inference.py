aceb981a216ea7b2ae962fdf884f7c0659a374aa
henriquefukushima <henrique.fukushima1@gmail.com>
2025-06-08 19:29:29 -0300

    first inference using yolov8x; poor ball tracking; needs to be fine tuned

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,9 +1,14 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8x') # Load a pretrained YOLOv8 model
+model = YOLO('models/yolov8x') # Load a pretrained YOLOv8 model
 
-model.predict(
-    'input_videos/input_video.mp4',
+result = model.predict(
+    'input_videos/input_video.mp4', 
     save=True,  # Save the output video with detections
     show=True,  # Display the output video with detections
-)  # Path to the input video)
+) 
+
+print(result)
+print("BOXES: ")
+for box in result[0].boxes:
+    print(f"Box: {box.xyxy}, Confidence: {box.conf}, Class: {box.cls}")
