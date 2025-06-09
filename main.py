ff67e974696b23c71896b66ea016e6040ca777f9
henriquefukushima <henrique.fukushima1@gmail.com>
2025-06-09 16:52:43 -0300

    implemented PlayerTracker class

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from utils import (read_video,
                    save_video)
+from trackers import (PlayerTracker)
+
 def main():
+    # Read Video
     input_video_path = "input_videos/input_video.mp4"
     video_frames = read_video(input_video_path)
 
+    # Track Players 
+    player_tracker = PlayerTracker(model_path="models/yolov8x.pt")
+    player_detections = player_tracker.detect_frames(video_frames)
+
+    # Draw Bounding Boxes
+    video_frames = player_tracker.draw_bboxes(video_frames, player_detections)
+
+    # Save Output Video
     save_video(video_frames, "output_videos/output_video.avi")
 
 if __name__ == "__main__":
